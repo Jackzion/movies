12f6bc238b52b945cf430c7dffd5931fed89a6d9
ziio <1934255852@qq.com>
2026-09-11 11:44:07 +0800

    feat(home): wire theme + Riverpod for images
    - mount createTheme() on MaterialApp
    
    - add Riverpod movieImagesProvider (riverpod_generator)
    
    - refactor HomeScreenImage to ConsumerWidget
    
    - fix ScrollView nesting (move into Scaffold.body)
    
    - apply theme text styles across home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movies/ui/main_screen.dart';
 import 'package:movies/ui/theme/theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatefulWidget {
